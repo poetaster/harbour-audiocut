78ce5e8118a8df5a138c3719333918d71fbf5405
Åke Engelbrektson <eson57@users.noreply.github.com>
2021-04-02 21:44:03 +0200

    Create harbour-audiocut-sv.ts
    Add Swedish translation

---
+++ translations/harbour-audiocut-sv.ts
@@ -0,0 +1,233 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name>FirstPage</name>
+    <message>
+        <source>Select audio file</source>
+        <translation>Välj ljudfil</translation>
+    </message>
+    <message>
+        <source>Info</source>
+        <translation>Info</translation>
+    </message>
+    <message>
+        <source>Files</source>
+        <translation>Filer</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Spara</translation>
+    </message>
+    <message>
+        <source>Audiocut</source>
+        <translation>Audiocut</translation>
+    </message>
+    <message>
+        <source>python3-pydub is not installed</source>
+        <translation>python3-pydub är inte installerad</translation>
+    </message>
+    <message>
+        <source>remove (trim marked)</source>
+        <translation>Ta bort (trimmarkerad)</translation>
+    </message>
+    <message>
+        <source>extract (trim unmarked)</source>
+        <translation>Extrahera (ej trimmarkerad)</translation>
+    </message>
+    <message>
+        <source>volume -/+</source>
+        <translation>Volym -/+</translation>
+    </message>
+    <message>
+        <source>fade in</source>
+        <translation>Tona in</translation>
+    </message>
+    <message>
+        <source>fade out</source>
+        <translation>Tona ut</translation>
+    </message>
+    <message>
+        <source>silence</source>
+        <translation>Tyst</translation>
+    </message>
+    <message>
+        <source> dB</source>
+        <translation> dB</translation>
+    </message>
+    <message>
+        <source>speed - ignore pitch</source>
+        <translation>Hastighet - Ignorera tonhöjd</translation>
+    </message>
+    <message>
+        <source>reverse (backwards)</source>
+        <translation>Omvänd (tillbaka)</translation>
+    </message>
+    <message>
+        <source>x </source>
+        <translation>x </translation>
+    </message>
+    <message>
+        <source>copy</source>
+        <translation>Kopiera</translation>
+    </message>
+    <message>
+        <source>paste (insert)</source>
+        <translation>Klistra in (infoga)</translation>
+    </message>
+    <message>
+        <source>paste (replace)</source>
+        <translation>Klistra in (ersätt)</translation>
+    </message>
+    <message>
+        <source>paste (overlay)</source>
+        <translation>Klistra in (överlägg)</translation>
+    </message>
+    <message>
+        <source>trim silence</source>
+        <translation>Trimma tystnad</translation>
+    </message>
+    <message>
+        <source>echo effect</source>
+        <translation>Ekoeffekt</translation>
+    </message>
+    <message>
+        <source>low-pass filter</source>
+        <translation>Lågpassfilter</translation>
+    </message>
+    <message>
+        <source>high-pass filter</source>
+        <translation>Högpassfilter</translation>
+    </message>
+    <message>
+        <source>type 1</source>
+        <translation>Typ 1</translation>
+    </message>
+    <message>
+        <source>type 2</source>
+        <translation>Typ 2</translation>
+    </message>
+    <message>
+        <source>double instruments</source>
+        <translation>Dubbla instrument</translation>
+    </message>
+    <message>
+        <source>mountain concert</source>
+        <translation>Bergskonsert</translation>
+    </message>
+    <message>
+        <source>robot style</source>
+        <translation>Robotstil</translation>
+    </message>
+    <message>
+        <source> dB threshold</source>
+        <translation> dB tröskelvärde</translation>
+    </message>
+    <message>
+        <source> sec duration</source>
+        <translation> sek varaktighet</translation>
+    </message>
+    <message>
+        <source>cutoff frequency (Hz)</source>
+        <translation>Brytfrekvens (Hz)</translation>
+    </message>
+    <message>
+        <source>rename</source>
+        <translation>Byt namn</translation>
+    </message>
+    <message>
+        <source>delete</source>
+        <translation>Ta bort</translation>
+    </message>
+    <message>
+        <source>Delete file?</source>
+        <translation>Vill du ta bort filen?</translation>
+    </message>
+    <message>
+        <source>speed - keep pitch</source>
+        <translation>Hastighet - Bevara tonhöjden</translation>
+    </message>
+    <message>
+        <source>denoise</source>
+        <translation>Denoise</translation>
+    </message>
+    <message>
+        <source>at beginning</source>
+        <translation>vid början</translation>
+    </message>
+    <message>
+        <source>at end</source>
+        <translation>vid slutet</translation>
+    </message>
+    <message>
+        <source>add silence</source>
+        <translation>Lägg till tystnad</translation>
+    </message>
+    <message>
+        <source>at cursor</source>
+        <translation>vid markör</translation>
+    </message>
+</context>
+<context>
+    <name>InfoPage</name>
+    <message>
+        <source>Info</source>
+        <translation>Info</translation>
+    </message>
+</context>
+<context>
+    <name>SavePage</name>
+    <message>
+        <source>Save as</source>
+        <translation>Spara som</translation>
+    </message>
+    <message>
+        <source>overwrite...</source>
+        <translation>Skriv över...</translation>
+    </message>
+    <message>
+        <source>Original Folder</source>
+        <translation>Ursprunglig mapp</translation>
+    </message>
+    <message>
+        <source>Path</source>
+        <translation>Sökväg</translation>
+    </message>
+    <message>
+        <source>Size</source>
+        <translation>Storlek</translation>
+    </message>
+    <message>
+        <source>Artist</source>
+        <translation>Artist</translation>
+    </message>
+    <message>
+        <source>Title</source>
+        <translation>Titel</translation>
+    </message>
+    <message>
+        <source>Album</source>
+        <translation>Album</translation>
+    </message>
+    <message>
+        <source>Year</source>
+        <translation>År</translation>
+    </message>
+    <message>
+        <source>Track#</source>
+        <translation>Spår#</translation>
+    </message>
+    <message>
+        <source>Source file</source>
+        <translation>Källfil</translation>
+    </message>
+    <message>
+        <source>Audio tags</source>
+        <translation>Ljudtaggar</translation>
+    </message>
+    <message>
+        <source>LAME encoder for mp3 is not yet installed.</source>
+        <translation>LAME kodare för mp3 är ännu inte installerad.</translation>
+    </message>
+</context>
+</TS>
